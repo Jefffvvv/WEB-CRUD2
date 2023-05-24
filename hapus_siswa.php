722ae51a81829f7f30fe06a84d81b8044823c7df
Jefffvvv <105692008+Jefffvvv@users.noreply.github.com>
2023-05-24 12:29:36 +0700

    Update hapus_siswa.php

--- hapus_siswa.php
+++ hapus_siswa.php
@@ -4,9 +4,9 @@
         $qry_hapus=mysqli_query($konn,"delete from siswa where
         id='".$_GET['id']."'");
         if($qry_hapus){
-            echo "<script>alert('Sukses menghapus siswa');location.href='siswa.php';</script>";
+            echo "<script>alert('Sukses menghapus buku');location.href='siswa.php';</script>";
         } else {
-            echo "<script>alert('Gagal menghapus siswa');location.href='siswa.php';</script>";
+            echo "<script>alert('Gagal menghapus buku');location.href='siswa.php';</script>";
         }
         }
 ?>
