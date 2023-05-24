28c645aa84fcfe3f2ed8653aac8c14e4414fdbe0
Jefffvvv <105692008+Jefffvvv@users.noreply.github.com>
2023-05-24 12:28:33 +0700

    Update edit_siswa.php

--- edit_siswa.php
+++ edit_siswa.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student edits</title>
+    <title>Edit Buku</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
@@ -18,7 +18,7 @@
     <br></br>
     <div class="container">
         <div class="card">
-            <h1 class="card-header">Student edits</h1>
+            <h1 class="card-header">Edit Buku</h1>
             <div class="card-body">
                 <form method="POST" action="proses_edit_siswa.php" enctype="multipart/form-data">
                     <input type="hidden" name="id" value="<?=$data_siswa['id']?>">
@@ -27,15 +27,15 @@
                         <input type="text" class="form-control" name="nama" value="<?=$data_siswa['nama']?>" placeholder="Masukkan nama" required>
                     </div>
                     <div class="mb-3">
-                        <label for="absen" class="form-label">Absence</label>
+                        <label for="absen" class="form-label">Jumlah Halaman</label>
                         <textarea class="form-control" name="absen" row="3" placeholder="Masukkan absen" required><?=$data_siswa['absen']?></textarea>
                     </div>
                     <div class="mb-3">
-                        <label for="kelas" class="form-label">Class</label>
+                        <label for="kelas" class="form-label">Kategori</label>
                         <input type="text" class="form-control" name="kelas" value="<?=$data_siswa['kelas']?>" placeholder="Masukkan kelas" required>
                     </div>
                     <div class="mb-3">
-                        <label for="foto" class="form-label">Picture</label>
+                        <label for="foto" class="form-label">Foto Buku</label>
                         <input type="file" class="form-control" name="foto">
                     </div>
                     <button type="submit" class="btn btn-primary">Update</button>
